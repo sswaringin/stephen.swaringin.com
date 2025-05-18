f351c51413dd8a7231c060edcf5e8bf71f612bed
Stephen Swaringin <stephen.swaringin@gmail.com>
2025-05-17 22:34:16 -0500

    adding css reset

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,7 @@
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPassthroughCopy('src/bundle.css');
+  // eleventyConfig.addPassthroughCopy('src/css/global');
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
