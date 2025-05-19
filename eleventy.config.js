0101190749bb987cecd416b06a58bc9cb64724ac
Stephen Swaringin <stephen.swaringin@gmail.com>
2025-05-18 23:58:20 -0500

    exploring collections

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,13 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/fonts');
   eleventyConfig.addPassthroughCopy('src/bundle.css');
   eleventyConfig.addPassthroughCopy('src/css');
+
+  /* posts collection */
+  eleventyConfig.addCollection('posts', (collection) => {
+    return collection
+      .getFilteredByGlob('./src/posts/*.md')
+      .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
+  });
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
---
+++ src/posts/post-0.md
@@ -0,0 +1,6 @@
+---
+tags: post
+title: 'Post 0'
+displayOrder: 0
+url: '/writing/post-0'
+---
