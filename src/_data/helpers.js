56567922d8c77c513ec7aa0c9673e3580fe8c80f
Stephen Swaringin <stephen.swaringin@gmail.com>
2025-05-21 06:57:32 -0500

    adding random other blog posts to footer

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -24,4 +24,42 @@ function getLinkActiveState(itemUrl, pageUrl) {
   return response;
 };
 
-export { getLinkActiveState, hello }
+/**
+ * Filters out the passed item from the passed collection
+ * and randomizes and limits them based on flags
+ * 
+ * @param {Array} collection The 11ty collection we want to take from
+ * @param {Object} item The item we want to exclude (often current page)
+ * @param {Number} limit=3 How many items we want back
+ * @param {Boolean} random=true Whether or not this should be randomized
+ * @returns {Array} The resulting collection
+ */
+function getSiblingContent(collection, item, limit = 3, random = true) {
+  let filteredItems = collection.filter(x => x.url !== item.url);
+
+  if (random) {
+    let counter = filteredItems.length;
+
+    while (counter > 0) {
+      // Pick a random index
+      let index = Math.floor(Math.random() * counter);
+
+      counter--;
+
+      let temp = filteredItems[counter];
+
+      // Swap the last element with the random one
+      filteredItems[counter] = filteredItems[index];
+      filteredItems[index] = temp;
+    }
+  }
+
+  // Lastly, trim to length
+  if (limit > 0) {
+    filteredItems = filteredItems.slice(0, limit);
+  }
+
+  return filteredItems;
+}
+
+export { getLinkActiveState, getSiblingContent, hello }
